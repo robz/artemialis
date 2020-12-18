38bc67e7beae718ca43648539d24b7496bf5b66b
robz <rjnevels@gmail.com>
2020-12-18 17:18:18 -0600

    record wavenet model parameters

--- check_wavenet_receptive_field.py
+++ check_wavenet_receptive_field.py
@@ -1,5 +1,21 @@
+import torch
+
 from Wavenet import Wavenet
 from MidiIndexUtils import NUM_CHANNELS
 
-wavenet = Wavenet(channels=NUM_CHANNELS, num_layers=10, num_stacks=2, kernel_size=2, dilation_rate=2, device='cpu')
+wavenet = Wavenet(input_channels=NUM_CHANNELS, output_channels=NUM_CHANNELS, hidden_channels=64, num_layers=10, num_stacks=2, kernel_size=2, dilation_rate=2, device='cpu')
+wavenet.load_state_dict(torch.load("Documents/PerformanceMidi/models/performance_wavenet-iter10-400.pt"))
 print(wavenet.receptive_field)
+
+wavenet = Wavenet(input_channels=NUM_CHANNELS, output_channels=NUM_CHANNELS, hidden_channels=64, num_layers=9, num_stacks=4, kernel_size=2, dilation_rate=2, device='cpu')
+wavenet.load_state_dict(torch.load("Documents/PerformanceMidi/models/performance_wavenet-iter11-400.pt"))
+print(wavenet.receptive_field)
+
+wavenet = Wavenet(input_channels=NUM_CHANNELS+128, output_channels=NUM_CHANNELS, hidden_channels=64, num_layers=10, num_stacks=2, kernel_size=2, dilation_rate=2, device='cpu')
+wavenet.load_state_dict(torch.load("Documents/PerformanceMidi/models/performance_wavenet-iter13-400.pt"))
+print(wavenet.receptive_field)
+
+wavenet = Wavenet(input_channels=NUM_CHANNELS+128, output_channels=NUM_CHANNELS, hidden_channels=64, num_layers=9, num_stacks=4, kernel_size=2, dilation_rate=2, device='cpu')
+wavenet.load_state_dict(torch.load("Documents/PerformanceMidi/models/performance_wavenet-iter14-1.pt"))
+print(wavenet.receptive_field)
+
